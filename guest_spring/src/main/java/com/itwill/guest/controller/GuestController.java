36df010a57fdefee08e412df8e2821bda165edd1
itwill <itwill@BOOK-UU5V8VKSL7>
2023-03-02 17:56:58 +0900

    ..

--- guest_spring/src/main/java/com/itwill/guest/controller/GuestController.java
+++ guest_spring/src/main/java/com/itwill/guest/controller/GuestController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itwill.guest.Guest;
@@ -22,87 +25,97 @@ public class GuestController {
 	}
 	@RequestMapping("/guest_main")
 	public String guest_main() {
-		String forwardPath = "forward:/WEB-INF/views/guest_main.jsp";
+		String forwardPath = "guest_main";
 		return forwardPath;
 	}
 	
 	@RequestMapping("/guest_list")
-	public String guest_list(HttpServletRequest request) throws Exception {
-		String forwardPath = "forward:/WEB-INF/views/guest_list.jsp";
+	public String guest_list(Model model) throws Exception {
 		List<Guest> guestList = guestDao.selectAll();
-		request.setAttribute("guestList", guestList);
+		model.addAttribute("guestList", guestList);
+		String forwardPath = "guest_list";
 		return forwardPath;
 	}
 	
 	@RequestMapping("/guest_error")
 	public String guest_error() {
-		String forwardPath = "redirect:guest_error";
+		String forwardPath = "guest_error";
 		return forwardPath;
 	}
-	@GetMapping("/guest_modify_action_get")
-	public String guest_modify_action_get(@ModelAttribute Guest guest) throws Exception {
-		guestDao.updateGuest(guest);
-		String forwardPath = "forward:/WEB-INF/views/guest_view.jsp";
-		return forwardPath;
-	}
-	@RequestMapping("/guest_modify_action")
+
+	@PostMapping("/guest_modify_action")
 	public String guest_modify_action(@ModelAttribute Guest guest) throws Exception {
 		guestDao.updateGuest(guest);
-		String forwardPath = "forward:/WEB-INF/views/guest_view.jsp";
+		String forwardPath = "guest_view";
+		return forwardPath;
+	}
+
+	@RequestMapping("/guest_modify_form")
+	public String guest_modify_form(Guest guest,Model model) throws Exception {
+		model.addAttribute(guest);
+		String forwardPath = "guest_modify_form";
+		return forwardPath;
+	}
+
+	@PostMapping("/guest_remove_action")
+	public String guest_remove_action(Guest guest) throws Exception {
+		guestDao.deleteGuest(guest.guest_no);
+		String forwardPath = "redirect:guest_list";
+		return forwardPath;
+	}
+
+	@RequestMapping("/guest_view")
+	public String guest_view(Guest guest,Model model) throws Exception {
+		int no = guest.guest_no;
+		model.addAttribute(guestDao.selectByNo(no));
+		String forwardPath = "guest_view";
+		return forwardPath;
+	}
+	@PostMapping("/guest_write_action")
+	public String guest_write_action(Guest guest) throws Exception {
+		guestDao.insertGuest(guest);
+		String forwardPath = "guest_view";
+		return forwardPath;
+	}
+	@RequestMapping("/guest_write_form")
+	public String guest_write_form() {
+		String forwardPath = "guest_write_form";
+		return forwardPath;
+
+	}
+	@GetMapping(value= {"guest_modify_form","guest_write_action","guest_remove_action","guest_modify_action"})
+	public String guest_get(){
+		String forwardPath="redirect:guest_main";
 		return forwardPath;
 	}
 	
+	
+	/*
 	@GetMapping("/guest_modify_get")
 	public String guest_modify_get(HttpServletRequest request) throws Exception {
 		int guest_no = Integer.parseInt((String) request.getAttribute("guest_no"));
 		Guest guest = guestDao.selectByNo(guest_no);
 		request.setAttribute("guest", guest);
-		String forwardPath = "forward:/WEB-INF/views/guest_modify_form.jsp";
+		String forwardPath = "guest_modify_form";
 		return forwardPath;
 	}
-	@RequestMapping("/guest_modify_get")
-	public String guest_modify_form(HttpServletRequest request) throws Exception {
-		int guest_no = Integer.parseInt((String) request.getAttribute("guest_no"));
-		Guest guest = guestDao.selectByNo(guest_no);
-		request.setAttribute("guest", guest);
-		String forwardPath = "forward:/WEB-INF/views/guest_modify_form.jsp";
-		return forwardPath;
-	}
-
-	public String guest_remove_action_get() {
+	@GetMapping("/guest_write_action_get")
+	public String guest_write_action_get(Model model) {
 		String forwardPath = "";
 		return forwardPath;
 	}
-
-	
-	public String guest_remove_action() {
-		String forwardPath = "";
-
+	@GetMapping("/guest_remove_action_get")
+	public String guest_remove_action_get(HttpServletRequest request) throws Exception {
+		int no = (int)request.getAttribute("guest_no");
+		guestDao.deleteGuest(no);
+		String forwardPath = "redirect:guest_list";
 		return forwardPath;
 	}
-
-	public String guest_view() {
-		String forwardPath = "";
-
+	@GetMapping("/guest_modify_action_get")
+	public String guest_modify_action_get(@ModelAttribute Guest guest) throws Exception {
+		guestDao.updateGuest(guest);
+		String forwardPath = "guest_view";
 		return forwardPath;
 	}
-
-	public String guest_write_action_get() {
-		String forwardPath = "";
-		return forwardPath;
-	}
-
-	public String guest_write_action() {
-		String forwardPath = "";
-
-		return forwardPath;
-	}
-
-	
-	public String guest_write() {
-		String forwardPath = "";
-		return forwardPath;
-
-	}
-
+	*/
 }
--- spring_web_application/src/main/java/com/itwill/controller/SpringCustomTagSPELController.java
+++ spring_web_application/src/main/java/com/itwill/controller/SpringCustomTagSPELController.java
@@ -2,6 +2,9 @@ package com.itwill.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +18,7 @@ public class SpringCustomTagSPELController {
 	public String jstl_fmt_i18n() {
 		return "jstl_fmt_i18n";
 	}
-	
+	@GetMapping("/spring_customtag_spel")
 	public String spring_customtag_spel(HttpServletRequest request) {
 		
 		request.setAttribute("price0", 234234);
@@ -38,15 +41,21 @@ public class SpringCustomTagSPELController {
 		return "spring_customtag_spel";
 	}
 	/************Spring JSP에서 국제화*******************
-	1. application-config.xml에 MessageSource빈설정
-	 <bean id="messageSource" class="org.springframework.context.support.ResourceBundleMessageSource">
-  		<property name="basenames" value="messages/messages,messages/user"/>
-  	 </bean>
+	1. ApplicationConfig.java에 MessageSource빈설정
+		 public class ApplicationConfig {
+		@Bean("messageSource")
+		public MessageSource messageSource() {
+			ResourceBundleMessageSource resourceBundleMessageSource=
+					new ResourceBundleMessageSource();
+			resourceBundleMessageSource.setBasenames("messages/messages","messages/user");
+			return resourceBundleMessageSource;
+		}
+	}
   	 *****************************************************/
 	
-	
+	@RequestMapping("/spring_customtag_spel_i18n")
 	public String spring_customtag_spel_i18n() {
-		return "";
+		return "spring_customtag_spel_i18n";
 	}
 	
 	
