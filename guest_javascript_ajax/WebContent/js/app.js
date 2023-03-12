ebbf831c9bdce6a52b5e62a3a157a1fbe7e61844
kangcarry <111220895+kangcarry@users.noreply.github.com>
2023-03-12 23:23:05 +0900

    .

--- guest_javascript_ajax/WebContent/js/app.js
+++ guest_javascript_ajax/WebContent/js/app.js
@@ -50,7 +50,7 @@ document.addEventListener('click',function(e){
 	/*****************guest_detail**************** */
 	if(e.target.classList.contains('guest_item_a')){
 		let params='guest_no='+e.target.getAttribute("guest_no");
-		let jsonResult = Service.guestService('GET',URL.GUEST_DETAL_URL,params);
+		let jsonResult = Service.guestService('GET',URL.GUEST_DETAIL_URL,params);
 		View.render("#guest-detail-template",jsonResult,"#content");
 	}
 	/*****************guest_delete_action**************** */
@@ -64,6 +64,15 @@ document.addEventListener('click',function(e){
 	}
 	}
 	/*****************guest_write_action**************** */
+	if(e.target.id=='btn_guest_write_action'){
+		
+		let jsonResult = Service.guestService('POST',URL.GUEST_WRITE_ACTION_URL,'');
+		if(jsonResult.code==1){
+			menuGuestList.click();
+		}else{
+			alert(jsonResult.msg);
+		}
+	}
 	/*****************guest_modify_form_action**************** */
 	/*****************guest_modify_action**************** */
 	e.preventDefault();
